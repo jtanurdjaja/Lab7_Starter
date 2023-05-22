f4955318069966d4ccd650914bf4b83c10a4304e
jtanurdjaja <97708996+jtanurdjaja@users.noreply.github.com>
2023-05-21 20:42:23 -0700

    Added serviceworker scope

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -52,7 +52,7 @@ function initializeServiceWorker() {
       // B3. TODO - Register './sw.js' as a service worker (The MDN article
       //            "Using Service Workers" will help you here)
       try {
-        await navigator.serviceWorker.register("./sw.js", );
+        await navigator.serviceWorker.register("./sw.js", {scope: "./"});
         // B4. TODO - Once the service worker has been successfully registered, console
         //            log that it was successful.
         console.log("successfully registered");
